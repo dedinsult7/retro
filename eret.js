8420d4603d55efb27a667b2443145e99bbbb1acd
dedinsult7 <utkirovsuhrob28@gmail.com>
2024-02-28 18:43:15 +0500

    retro

---
+++ eret.js
@@ -0,0 +1,19 @@
+
+
+alert('sizni random soniz')
+let a=Math.round(Math.random( )  * 99+1 ) 
+
+alert(a)
+
+
+let b=a/2
+let e=a*2
+
+// alert(e)
+// alert(b)
+
+alert(`
+random soni ${a} edi
+biz uni 2 ga ko'paytirib ${e} sonini ciqardik
+biz uni 2 ga bo'lib ${e} sonini ciqardik
+`)
